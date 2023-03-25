ad2aec148a3c98953f0a73b6b82d60bc559a64e3
AlisherovEsen <alischerov.esen@gmail.com>
2023-03-25 20:59:50 +0600

    comment

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.mobile-menu').on('click',function(e){
+    e.preventDefault()
+    $('.menu-btn').toggleClass('menu_active');
+    $('.menu-wrap').toggleClass('menu_active');
+    //$('.body').toggleClass('hidden');
+})
